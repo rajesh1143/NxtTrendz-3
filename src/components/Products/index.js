f277cc19a3f498ad0080712d27d186d96681943e
rajeshkumar <rajeshkumar.p9025@gmail.com>
2023-05-02 12:46:27 +0000

    sourcecode

---
+++ src/components/Products/index.js
@@ -0,0 +1,14 @@
+import Header from '../Header'
+import AllProductsSection from '../AllProductsSection'
+import './index.css'
+
+const Products = () => (
+  <>
+    <Header />
+    <div className="products-sections">
+      <AllProductsSection />
+    </div>
+  </>
+)
+
+export default Products
